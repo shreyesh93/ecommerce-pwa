f5d491a588702abc8c6da2d69b5c741605f182f7
shreyesh93 <shreyesh.rane93@gmail.com>
2025-04-06 02:04:59 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(reg => console.log('[Main] Service Worker Registered:', reg))
+      .catch(err => console.error('[Main] Service Worker Registration Failed:', err));
+  });
+}
+
+// Optional: measuring performance
+reportWebVitals();
